b28580e53aad248ca8683b64241aa10638522477
greimer29 <greimerabelguzman29@gmail.com>
2024-10-28 13:19:16 -0400

    adding real data to the verifyTime method

--- TimeVerify.js
+++ TimeVerify.js
@@ -1,22 +1,22 @@
-module.exports = function verifyTime (timeOfTimes) {
+module.exports = function verifyTime(timeOfTimes) {
 
     let timeIn, lunchStart, lunchEnd, dinnerStart, dinnerEnd, timeOut
     let daysWorkeds = []
 
-    const verifyWorkedPeriod = (timeIn,timeOut) => {
-        function hoursToMinuts (hour){
+    const verifyWorkedPeriod = (timeIn, timeOut) => {
+        function hoursToMinuts(hour) {
             //destructurar la hora y convertirlo de string a int
-            const [hours,minuts,period] = hour.split(/:| /)
+            const [hours, minuts, period] = hour.split(/:| /)
             const theHours = parseInt(hours)
             const theMinuts = parseInt(minuts)
 
             //calcular horas en base a minutos
             let totalMinuts = theHours * 60 + theMinuts
-        
+
             //validaciones para transformar horas a formato de 24 horas
-            if(period === 'pm' && theHours !== 12){
+            if (period === 'pm' && theHours !== 12) {
                 totalMinuts += 720
-            } else if (period === 'am' && theHours === 12){
+            } else if (period === 'am' && theHours === 12) {
                 totalMinuts = minuts
             }
 
@@ -27,7 +27,12 @@ module.exports = function verifyTime (timeOfTimes) {
         const minutsTimeOut = hoursToMinuts(timeOut)
 
         const workedPeriod = minutsTimeOut - minutsTimeIn
-        return workedPeriod
+        if (workedPeriod >= 300) {
+            //trabajo mas de 5 horas detener procesosretornar false
+            return false
+        }else{
+            return 'rela'
+        }
     }
 
     const validateTime = (times) => {
@@ -38,68 +43,79 @@ module.exports = function verifyTime (timeOfTimes) {
         dinnerEnd = times[4]
         timeOut = times[5]
 
-        if(lunchStart ){
-            console.log('Si tomo su hora de lunch')
-            const workedPeriod = verifyWorkedPeriod(timeIn,lunchStart)
+        if (timeIn) {
+            if (lunchStart) {
+                const workedPeriod = verifyWorkedPeriod(timeIn, lunchStart)
 
-            if(workedPeriod >= 300){
-                console.log('El pana trabajo mas de sus 5 horas seguidas')
-                //trabajo mas de 5 horas detener procesosretornar false
-                return false
-            }
-        }
-        if(lunchEnd && dinnerStart){
-            console.log('Si tomo su hora de cena')
-            const workedPeriod = verifyWorkedPeriod(lunchEnd,dinnerStart)
+                if (!workedPeriod) {
+                    return false
+                }
+            } else if (!lunchStart || !lunchEnd) {
+                if (dinnerStart) {
+                    const workedPeriod = verifyWorkedPeriod(timeIn, dinnerStart)
 
-            if(workedPeriod >= 300){
-                console.log('El pana trabajo mas de sus 5 horas seguidas')
-                return false
-            }
-            //si no tomo su cena pero no trbajo 5 horas continua 
-        } else if (lunchEnd && !dinnerStart){
-            console.log('No tomo su hora de cena')
-            const workedPeriod = verifyWorkedPeriod(lunchEnd,timeOut)
+                    if (!workedPeriod) {
+                        return false
+                    }
+                }else if (!dinnerStart || !dinnerEnd){
+                    const workedPeriod = verifyWorkedPeriod(timeIn, timeOut)
 
-            if(workedPeriod >= 300){
-                console.log('El pana trabajo mas de sus 5 horas seguidas')
-                return false
+                    if (!workedPeriod) {
+                        return false
+                    }
+                }
             }
+            if (lunchEnd && dinnerStart) {
+                const workedPeriod = verifyWorkedPeriod(lunchEnd, dinnerStart)
+
+                if (!workedPeriod) {
+                    return false
+                }
+            } else if (lunchEnd && !dinnerStart) {
+                const workedPeriod = verifyWorkedPeriod(lunchEnd, timeOut)
+
+                if (!workedPeriod) {
+                    return false
+                }
+            }
+
         }
         return true
     }
 
     timeOfTimes.forEach(day => {
         const dayworked = validateTime(day)
-        if(!dayworked){
+        if (!dayworked) {
             daysWorkeds.push(...[dayworked])
-        }else{
+        } else {
             daysWorkeds.push(...[dayworked])
         }
     });
 
+    return daysWorkeds
+
 }
 
 //funcion de prueba inicial no usar 
-function verifyTime1(){
+function verifyTime1() {
 
     console.log(timeOfTimes)
 
     //Convertir las horas a minutos tomando en cuenta el periodo am / pm
-    function hoursToMinuts (hour){
-        return 
+    function hoursToMinuts(hour) {
+        return
         //destructurar la hora y convertirlo de string a int
-        const [hours,minuts,period] = hour.split(/:| /)
+        const [hours, minuts, period] = hour.split(/:| /)
         const theHours = parseInt(hours)
         const theMinuts = parseInt(minuts)
 
         //calcular horas en base a minutos
         let totalMinuts = theHours * 60 + theMinuts
-    
+
         //validaciones para transformar horas a formato de 24 horas
-        if(period === 'pm' && theHours !== 12){
+        if (period === 'pm' && theHours !== 12) {
             totalMinuts += 720
-        } else if (period === 'am' && theHours === 12){
+        } else if (period === 'am' && theHours === 12) {
             totalMinuts = minuts
         }
 
@@ -114,7 +130,7 @@ function verifyTime1(){
     const workedMinuts = minutsTimeOut - minutsTimeIn
 
     //validamos si trabajo 5 horas 300mts === 5hrs
-    if(workedMinuts >= 300) {
+    if (workedMinuts >= 300) {
         console.log('Co;o marico has trabajado mas de 5 horas tu le hechas bola')
     }
 
--- exec.js
+++ exec.js
@@ -1,22 +1,14 @@
 const verifyTime = require("./TimeVerify.js");
+const cloking_example = require("./clockin-sample-2.json")
 
 const timeOfTimes = [
-    [
-        '8:00 am',
-        '11:00 am',
-        '12:00 pm',
-        '3:00 pm',
-        '4:00 pm',
-        '6:00 pm',
-    ],
-    [
-        '8:00 am',
-        '2:00 pm',
-        '2:30 pm',
-        null,
-        null,
-        '6:00 pm',
-    ]
 ]
 
-verifyTime(timeOfTimes)
+const jsonData = Object.keys(cloking_example)
+
+jsonData.forEach(day => {
+    timeOfTimes.push(cloking_example[day])
+})
+
+const daysWorkeds = verifyTime(timeOfTimes)
+console.log(daysWorkeds)
